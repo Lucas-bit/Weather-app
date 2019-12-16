448f084d6e159ad3f38511fb644b82358576f537
Lucas Gonzalez <lucasbertaudgonzalez@gmail.com>
2019-12-16 15:27:14 -0500

    got api key to return data in console

--- javascript.js
+++ javascript.js
@@ -2,33 +2,54 @@
  * pulls information from the form and build the query URL
  * @returns {string} URL for Openweather API based on form inputs
  */
-
-    var city = $("search-term").val();
+    
+var cities = [""];
+ 
+ $("#run-search").on("click", function(){
+     event.preventDefault()
+    var city = $("#search-value").val();
     // queryURL is the url we'll use to query the API//
-       
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&apikey=bc01722bf0252f19e97faf6059dd8e91";
-  
-    // Begin building an object to contain our API call's query parameters
-    // Set the API key
-    var queryParams = { "api-key": "bc01722bf0252f19e97faf6059dd8e91" };
-  
-    // Grab text the user typed into the search input, add to the queryParams object
-    queryParams.q = $("#search-term")
-      .val()
-      .trim();
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&APPID=bc01722bf0252f19e97faf6059dd8e91";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(data) {
+        console.log(data)   
+    })
+
+})
+
+// function renderCityButtons(){
+//     $("#search-value").empty()
     
-    
-  
-    console.log(queryParams)
+//     for (var i = 0; i < cities.length; i++){
+
+//         var a =("<button>");
+
+//         a.addClass=("city");
+
+//         a.attr("city-name", cities[i]);
+
+//         a.text(cities[i]);
+
+//         $("searched-cities").append(a)
+//     }
+// }
+
+
+
+// // ,
+   
   
 
-  // Function to empty out the articles
-  function clear() {
-    $("#article-section").empty();
-  }
+// //   // Function to empty out the forecast
+// //   function clear() {
+// //     $("#forecast-section").empty();
+// //   }
   
-  // CLICK HANDLERS
-  // ==========================================================
+// //   // CLICK HANDLERS
+// //   // ==========================================================
   
   // .on("click") function associated with the Search Button
   $("#run-search").on("click", function(event) {
@@ -36,21 +57,21 @@
     // This way we can hit enter on the keyboard and it registers the search
     // (in addition to clicks). Prevents the page from reloading on form submit.
     event.preventDefault();
+  })
   
-    // Empty the region associated with the articles
-    clear();
+// //     // Empty the region associated with the forecast
+// //     clear();
   
-    // Build the query URL for the ajax request to the OpenWeather API
+// //     // Build the query URL for the ajax request to the OpenWeather API
     
   
-    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
-    // The data then gets passed as an argument to the updatePage function
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(updatePage);
-  });
+// //     // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+// //     // The data then gets passed as an argument to the updatePage function
+//     $.ajax({
+//       url: queryURL,
+//       method: "GET"
+//     }).then(updatePage);
+//   ;
   
-  //  .on("click") function associated with the clear button
-  $("#clear-all").on("click", clear);
-  
+// //   //  .on("click") function associated with the clear button
+// //   $("#clear-all").on("click", clear);
